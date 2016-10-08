75a4fecb8685ac8a296cb2943408a6e9bd83367b
takatori <takatorisatoshi@team-lab.com>
2016-10-08 16:39:55 +0900

    ADD EX3.6

---
+++ src/main/scala/io/github/takatori/FunctionalProgrammingInScala/EX3/ex3_6.scala
@@ -0,0 +1,15 @@
+package io.github.takatori.FunctionalProgrammingInScala.EX3
+
+/**
+  * Created by takatorisatoshi on 2016/10/08.
+  */
+object ex3_6 {
+  def main(args: Array[String]): Unit = {
+    println(init(List(1,2,3,4,5)))
+  }
+
+  def init[A](l: List[A]): List[A] = l match {
+    case Cons(x, Nil) => Nil
+    case Cons(x, xs) => Cons(x, init(xs))
+  }
+}
