2685491b113a6ffd6af87b0a4860a821c40b15ca
takatori <takatorisatoshi@team-lab.com>
2016-10-08 16:33:51 +0900

    ADD EX3.5

---
+++ src/main/scala/io/github/takatori/FunctionalProgrammingInScala/EX3/ex3_5.scala
@@ -0,0 +1,18 @@
+package io.github.takatori.FunctionalProgrammingInScala.EX3
+
+/**
+  * Created by takatorisatoshi on 2016/10/08.
+  */
+object ex3_5 {
+  def main(args: Array[String]): Unit = {
+    println(dropWhile(List(1,2,3,4,5), (i: Int) => i < 3))
+  }
+
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = {
+    l match {
+      case Cons(x, xs) if f(x) => dropWhile(xs, f)
+      case Nil => Nil
+      case _ => l
+    }
+  }
+}
