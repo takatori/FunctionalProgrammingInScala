e72bee767a4a9e7ef0559eea5e25d725d9fb2180
takatori <takatorisatoshi@team-lab.com>
2016-10-08 16:13:10 +0900

    Fix EX2.4

--- src/main/scala/io/github/takatori/FunctionalProgrammingInScala/EX2/ex2_4.scala
+++ src/main/scala/io/github/takatori/FunctionalProgrammingInScala/EX2/ex2_4.scala
@@ -4,5 +4,5 @@ package io.github.takatori.FunctionalProgrammingInScala.EX2
   * Created by takatorisatoshi on 2016/10/08.
   */
 object ex2_4 {
-  def uncurry[A,B,C](f: B => C, g: A => B): (A,B) => C = (a: A, b: B) => f(g(a), b)
+  def uncurry[A,B,C](f: A => B => C): (A,B) => C = (a: A, b: B) => f(a)(b)
 }
