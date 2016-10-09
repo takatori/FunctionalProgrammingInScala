55ae0d482d020e0165bfb7a1dba839347d45ee70
takatori <takatorisatoshi@team-lab.com>
2016-10-09 14:17:18 +0900

    ADD EX4.2

---
+++ src/main/scala/io/github/takatori/FunctionalProgrammingInScala/EX4/ex4_2.scala
@@ -0,0 +1,17 @@
+package io.github.takatori.FunctionalProgrammingInScala.EX4
+
+/**
+  * Created by takatorisatoshi on 2016/10/08.
+  */
+object ex4_2 {
+
+  def mean(xs: Seq[Double]): Option[Double] =  {
+    if(xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+  }
+
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs) flatMap(m => mean(xs.map(x => math.pow((x - m), 2))))
+  }
+}
+
