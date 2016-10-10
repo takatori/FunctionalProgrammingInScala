813f1d54eac216c152628bdc2f6f45566843aebe
takatori <takatorisatoshi@team-lab.com>
2016-10-11 08:58:16 +0900

    ADD EX4.5

---
+++ src/main/scala/io/github/takatori/FunctionalProgrammingInScala/EX4/ex4_5.scala
@@ -0,0 +1,11 @@
+package io.github.takatori.FunctionalProgrammingInScala.EX4
+
+/**
+  * Created by takatorisatoshi on 2016/10/11.
+  */
+object ex4_5 {
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C):Option[C] = a.flatMap(sa => b.map(sb => f(sa,sb)))
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a.foldRight[Option[List[B]]](Some(Nil))((x,y) => map2(f(x),y)(_ :: _))
+}
