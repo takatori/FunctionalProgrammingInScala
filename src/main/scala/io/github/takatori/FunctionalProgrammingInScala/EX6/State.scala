b14955773a82e19c4d428ceb2027527992578925
takatori <takatorisatoshi@team-lab.com>
2016-10-28 23:29:40 +0900

    ADD EX6

--- src/main/scala/io/github/takatori/FunctionalProgrammingInScala/EX6/State.scala
+++ src/main/scala/io/github/takatori/FunctionalProgrammingInScala/EX6/State.scala
@@ -6,14 +6,18 @@ trait RNG {
   type Rand[+A] = RNG => (A, RNG)
 }
 
-case class SimpleRNG(seed: Long) extends RNG {
-  def nextInt: (Int, RNG) = {
-    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFL
-    val nextRNG = SimpleRNG(newSeed)
-    val n = (newSeed >> 16).toInt
-    (n, nextRNG)
+object RNG {
+
+  case class SimpleRNG(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFL
+      val nextRNG = SimpleRNG(newSeed)
+      val n = (newSeed >> 16).toInt
+      (n, nextRNG)
+    }
   }
+
+  type Rand[+A] = RNG => (A, RNG)
+
+  val int: Rand[Int] = _.nextInt
 }
-
-
-
